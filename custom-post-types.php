d1d2ee6b94671232f8bc004d7fad9bc5947d8d51
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-14 02:28:47 +0330

    Create custom-post-types.php

---
+++ custom-post-types.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Custom Post Types
+Description: Generate Custom Post Types For Wordpress Very Easy!!
+Version: 1.0
+Author: Omid Beheshtian
+*/
+
+// Activation and Deactivation Hooks
+register_activation_hook(__FILE__, 'custom_post_types_activation_function');
+register_deactivation_hook(__FILE__, 'custom_post_types_deactivation_function');
+
+// Include necessary files
+include_once(plugin_dir_path(__FILE__) . 'includes/admin.php');
+include_once(plugin_dir_path(__FILE__) . 'includes/functions.php');
+include_once(plugin_dir_path(__FILE__) . 'includes/settings.php');
