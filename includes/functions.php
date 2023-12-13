f5b1de32f46c3990d58a495750e5034e148a3110
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-14 02:30:27 +0330

    Update functions.php to be a plugin

--- includes/functions.php
+++ includes/functions.php
@@ -1,59 +1,23 @@
 <?php
-add_action('init', 'example_post_type');
+// Hook into the init action and create custom post type
+add_action('init', 'create_custom_post_type');
 
-function example_post_type() {
+function create_custom_post_type() {
+    $options = get_option('custom_post_types_options');
+    $post_type_name = isset($options['post_type_name']) ? $options['post_type_name'] : 'custom_post_type';
+
     $labels = array(
-        'name' => __('Examples'),
-        'singular_name' => __('Example'),
-        'add_new' => __('Add New Example'),
-        'add_new_item' => __('Add New Example'),
-        'edit_item' => __('Edit Example'),
-        'new_item' => __('New Example'),
-        'all_items' => __('All Examples'),
-        'view_item' => __('View Example'),
-        'search_items' => __('Search Examples'),
-        'not_found' => __('No examples found'),
-        'not_found_in_trash' => __('No examples found in Trash'),
-        'parent_item_colon' => '',
-        'menu_name' => __('Examples'),
+        'name' => ucfirst($post_type_name),
+        'singular_name' => ucfirst($post_type_name),
+        // Add other labels as needed
     );
 
     $args = array(
         'labels' => $labels,
-        'description' => __('Example'),
-        'rewrite' => array('slug' => 'example'),
         'public' => true,
         'has_archive' => true,
-        'show_in_nav_menus' => false,
-        'menu_icon' => 'dashicons-megaphone',
-        'supports' => array(
-            'title',
-            'editor',
-            'excerpt',
-            'trackbacks',
-            'custom-fields',
-            'comments',
-            'revisions',
-            'thumbnail',
-            'author',
-            'page-attributes'
-        )
+        // Add other arguments as needed
     );
 
-    register_post_type('example', $args);
+    register_post_type($post_type_name, $args);
 }
-
-// Register taxonomy
-function create_example_tax() {
-    register_taxonomy(
-        'type',
-        'example',
-        array(
-            'label' => __('Example Type'),
-            'rewrite' => array('slug' => 'example-type'),
-            'hierarchical' => true,
-        )
-    );
-}
-
-add_action('init', 'create_example_tax');
