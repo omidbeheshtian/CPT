5b4a15051e83ba2f709de58026fd57af41c23e41
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-14 02:18:49 +0330

    Activation and Deactivation Hooks

--- custom-post-type.php
+++ custom-post-type.php
@@ -5,3 +5,7 @@ Description: Generate Custom Post Types For Wordpress
 Version: 1.0
 Author: Omid Beheshtian
 */
+
+// Activation and Deactivation Hooks
+register_activation_hook(__FILE__, 'your_plugin_activation_function');
+register_deactivation_hook(__FILE__, 'your_plugin_deactivation_function');
