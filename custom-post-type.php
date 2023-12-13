c2507b81efd5961d72f082fbcb3826dca3a2737a
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-14 02:25:31 +0330

    Update custom-post-type.php

--- custom-post-type.php
+++ custom-post-type.php
@@ -13,3 +13,4 @@ register_deactivation_hook(__FILE__, 'custom-post-type_deactivation_function');
 // Include necessary files
 include_once(plugin_dir_path(__FILE__) . 'includes/admin.php');
 include_once(plugin_dir_path(__FILE__) . 'includes/functions.php');
+include_once(plugin_dir_path(__FILE__) . 'includes/settings.php');
