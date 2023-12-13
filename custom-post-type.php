8e304e0d348942bb0b603ebb392406e66f417065
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-14 02:19:08 +0330

    Revert "Activation and Deactivation Hooks"
    This reverts commit 5b4a15051e83ba2f709de58026fd57af41c23e41.

--- custom-post-type.php
+++ custom-post-type.php
@@ -5,7 +5,3 @@ Description: Generate Custom Post Types For Wordpress
 Version: 1.0
 Author: Omid Beheshtian
 */
-
-// Activation and Deactivation Hooks
-register_activation_hook(__FILE__, 'your_plugin_activation_function');
-register_deactivation_hook(__FILE__, 'your_plugin_deactivation_function');
