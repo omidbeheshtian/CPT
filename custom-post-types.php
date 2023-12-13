0a0aebc560d8abccbe853faf95f58dd6050946c3
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-14 02:39:20 +0330

    Bug Fixed!
    The Bug Was:
    The plugin generated 22 characters of unexpected output during activation. If you notice “headers already sent” messages, problems with syndication feeds or other issues, try deactivating or removing this plugin.
    
    AND:
    
    The plugin generated 310 characters of unexpected output during activation. If you notice “headers already sent” messages, problems with syndication feeds or other issues, try deactivating or removing this plugin.

--- custom-post-types.php
+++ custom-post-types.php
@@ -1,8 +1,4 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 ob_start();
 /*
 Plugin Name: Custom Post Types
@@ -11,13 +7,8 @@ Version: 1.0
 Author: Omid Beheshtian
 */
 
-// Activation and Deactivation Hooks
-register_activation_hook(__FILE__, 'custom_post_types_activation_function');
-register_deactivation_hook(__FILE__, 'custom_post_types_deactivation_function');
-
 // Include necessary files
 include_once(plugin_dir_path(__FILE__) . 'includes/admin.php');
 include_once(plugin_dir_path(__FILE__) . 'includes/functions.php');
-include_once(plugin_dir_path(__FILE__) . 'includes/settings.php');
 
 ob_end_flush();
