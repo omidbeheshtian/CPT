7bf44fe0db82dbb5df475b79b90f98b131ad1593
Omid Beheshtian <beheshtian.omid82@gmail.com>
2023-12-20 17:11:25 +0330

    Add taxonomy support when registering custom post type

--- includes/functions.php
+++ includes/functions.php
@@ -2,6 +2,7 @@
 // Hook into the init action and create custom post type
 add_action('init', 'create_custom_post_type');
 
+// Add taxonomy support when registering custom post type
 function create_custom_post_type() {
     $options = get_option('custom_post_types_options');
     $post_type_name = isset($options['post_type_name']) ? $options['post_type_name'] : 'custom_post_type';
@@ -16,8 +17,11 @@ function create_custom_post_type() {
         'labels' => $labels,
         'public' => true,
         'has_archive' => true,
+        'taxonomies' => array('custom_taxonomy'), // Add this line for taxonomy support
         // Add other arguments as needed
     );
 
     register_post_type($post_type_name, $args);
 }
+add_action('init', 'create_custom_post_type');
+
